acedbf7be414f51086dce3dc891ae2915d6879e3
jaemyung.kim <jaemyung.kim@navercorp.com>
2021-01-03 22:26:30 +0900

    tabulator

--- client/src/main/scala/org/github/windbird123/play/monadic/tabulatorTab/MyTable.scala
+++ client/src/main/scala/org/github/windbird123/play/monadic/tabulatorTab/MyTable.scala
@@ -1,44 +1,46 @@
 package org.github.windbird123.play.monadic.tabulatorTab
 
 import mhtml._
-import org.scalajs.dom.{document, window}
-import org.scalajs.dom.raw.{Event, HTMLElement}
+import org.scalajs.dom.{ document, window }
+import org.scalajs.dom.raw.{ Event, HTMLElement }
 
 import scala.scalajs.js
-import scala.scalajs.js.annotation.{JSExport, JSExportTopLevel}
+import scala.scalajs.js.annotation.{ JSExport, JSExportTopLevel }
 
 @JSExportTopLevel("MyTable")
 object MyTable {
 
   @JSExport
   def init(id: String, query: String): Unit = {
-    var tabulator : Tabulator = null
-
+    var tabulator: Tabulator = null
 
     // 아래처럼 js.Array 로 js.Object 를 생성해도 되지만,
     // json string 을 그대로 넘겨도 된다 !!!!
 
 //    val rowToAdd =  js.Array(js.Dictionary[Any]("id" -> "3", "name" -> "windbird", "age" -> "22"))
     val rowToAdd =
-        s"""{
-           |  "id": "3",
-           |  "name": "windbird",
-           |  "age": "22"
-           |}
-           |""".stripMargin.asInstanceOf[js.Object]
-
+      s"""{
+         |  "id": "3",
+         |  "name": "windbird",
+         |  "age": "22"
+         |}
+         |""".stripMargin.asInstanceOf[js.Object]
 
     val content =
       <div>
         <button id="add-row" onclick={() => tabulator.addRow(rowToAdd, true); ()}>Add windbird Row to top</button>
-        <button id="del-row" onclick={ () => tabulator.deleteRow("2".asInstanceOf[js.Object])}>Delete Row "Oli Bob"</button>
+        <button id="del-row" onclick={() => tabulator.deleteRow("2".asInstanceOf[js.Object])}>Delete Row "Oli Bob"</button>
         <div id="example-table"></div>
       </div>
 
     mount(document.getElementById(id), content)
 
-    val rowClickFunction: js.Function2[Event, RowComponent, Unit] = (e: Event, row: RowComponent) =>
+    val rowClickFunction: js.Function2[Event, RowComponent, Unit] = (e: Event, row: RowComponent) => {
+      val name = row.getCell("name").getValue()
+      println(name)
+      row.getCells().foreach(cell => println(cell.getField() + ": " + cell.getValue()))
       window.alert(row.getData().asInstanceOf[HTMLElement].id + " was clicked")
+    }
 
     val tableData = js.Array(js.Dictionary[Any]("id" -> "2", "name" -> "Oli Bob", "age" -> "12"))
 
@@ -47,7 +49,7 @@ object MyTable {
       "data"   -> tableData,
       "layout" -> "fitColumns",
       "columns" -> js.Array(
-        js.Dictionary[Any]("title" -> "Name", "field" -> "name", "width"   -> 150),
+        js.Dictionary[Any]("title" -> "Name", "field" -> "name", "width"   -> 150, "editor" -> "input"),
         js.Dictionary[Any]("title" -> "Age", "field"  -> "age", "hozAlign" -> "left", "formatter" -> "progress")
       ),
       "rowClick" -> rowClickFunction
